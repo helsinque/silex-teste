bcb3d7aaf489514b70539b21a9b7477244f52491
Eduardo Galbiati <eduardo.galbiati7@gmail.com>
2014-06-16 20:17:37 -0300

    Segundo commit, Criação de Login e modelo de cadastro de CRUD

--- bootstrap-test.php
+++ bootstrap-test.php
@@ -4,6 +4,9 @@
  *
  * The global configuration for the tests.
  */
+if(! defined("ROOT")):
+    define("ROOT", (__DIR__));
+endif;
 
 error_reporting(E_ALL | E_STRICT);
 
@@ -12,3 +15,17 @@ ini_set('display_errors', 1);
 date_default_timezone_set('America/Sao_Paulo');
 
 include_once 'vendor/autoload.php';
+/**
+ * crée une application et configure celle-ci.
+ * @return Silex\Application
+ */
+function createApplication() {
+    //putenv("MARKME_DB_DRIVER=pdo_sqlite");
+   // $schema = file_get_contents(ROOT . '/Database/schema.sqlite.sql');
+    $app = require  ROOT.'/web/index.php';
+    $app["debug"] = true;
+    //$app["exception_handler"]->disable();
+    //$app["session.test"] = true;
+    //$app["db"]->exec($schema);
+    return $app;
+}
--- web/index.php
+++ web/index.php
@@ -3,64 +3,129 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Silex\Provider\TranslationServiceProvider;
 use Silex\Application;
 
 $app = new Application();
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'dbs.options' => array(
-        'teste' => array(
-          'driver'   => 'pdo_sqlsrv',
-          'path'     => __DIR__.'/app.db',
-          'host'      => 'localhost',
-          'dbname'    => 'teste',
-          'user'      => 'sa',
-          'password'  => 'senha123',
-          'charset'   => 'utf8',
-        ),
-        
-    ),
-));
-/*
-$app->register(new Silex\Provider\SecurityServiceProvider(), array(
-    'security.firewalls' => array(
-    'admin' => array(
-        'pattern' => '^/',
-        'http' => true,
-        'users' => $app->share(function () use ($app) {
-            return new Bpms\Login\Controller\UserProvider($app['dbs']);
-        }),
-        ),
-    )
-));
-*/
-//Alterar este método horrivÊ&nbsp;!
-/*
-$request = new Request;
+require_once dirname(__DIR__).'/src/config.php';
+require_once dirname(__DIR__).'/src/services.php';
 
-$app->get('/{module}/{controller}/{action}', function ($module, $controller, $action ) use ( $app , $request ){
-  $className = 'Bpms\\'.$module.'\\Controller\\'.$controller.'Controller';
+if ($app['debug'] == false) {
+    $app->error(function (\Exception $e, $code) {
+        switch ($code) {
+            case 404:
+                $message = 'The requested page could not be found.';
+                break;
+            case 403:
+                $message = 'You dont have permission to access this method';
+                break;
+            default:
+                $message = 'We are sorry, but something went terribly wrong.';
+        }
 
-  try{
-     $newController = new $className($app,$request);
+        return new Response($message);
+    });
+}
+
+$app->get( '/', function () use ($app) {
+    return new Response($app['twig']->render('bpms/Index/views/index.html.twig', array()));
+})->bind('index');
+
+
+$app->get('/login', function(Request $request) use ($app) {
+    return $app['twig']->render('Core/Login/Views/login.html.twig', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
+});
+
+$app->get('/logout', function () use ($app){
+  $app['session']->clear();
+  return $app->redirect($app['url_generator']->generate('index'));
+})->bind('logout');
+
+$app->get('/users/crud/new', function () use ($app){
+  $class = new Core\Users\Controllers\UserCrudController($app);
+  return $class->NewForm();
+})->bind('users.new');
+
+$app->post('/users/crud/save', function () use ($app){
+  $class = new Core\Users\Controllers\UserCrudController($app);
+  return $class->saveAction();
+})->bind('users.save');
+
+
+
+/*
+
+
+$app->get('/{module}/{controller}/{action}', function ($module, $controller, $action ) use ( $app ){
+    $className = 'Bpms\\'.$module.'\\Controller\\'.$controller.'Controller';
+     $newController = new $className($app);
    }catch (Excetption $e){
      echo 'Erro ao chamar o controller';
    }
   //  $controller = new
   return 'asé';
 });
+$app->get( '/users', function () use ($app) {
+    if ($app['crud.permission']->hasCreatePermissionForModule('index'))
+       return new Response($app['twig']->render('index.html.twig', array()));
+       //return $app['twig']->render('index.html.twig');
+    else
+        return 'Voce não tem permissão para acessar esta funcionalidade';
+})->bind('users');
 
-$app->get( '/Sequencia/Os/Consulta/{idOs}',  function ($idOs) use ( $app , $request ){
+$app->get( '/departments', function () use ($app) {
+    if ($app['crud.permission']->hasCreatePermissionForModule('index'))
+       return new Response($app['twig']->render('index.html.twig', array()));
+       //return $app['twig']->render('index.html.twig');
+    else
+        return 'Voce não tem permissão para acessar esta funcionalidade';
+})->bind('departments');
 
-       $newController = new Bpms\Sequencia\Controller\OsController( $app['dbs'] );
 
-       $return = $newController->consultaOs($idOs);
-       return $app->json( $return );
+$app->get( '/{module}/crud/new', function ($module) use ($app) {
 
-} );
-*/
-$app['debug'] = true;
-//Funciona com o TESTECONSTROLLER
+    if ($app['crud.permission']->hasCreatePermissionForModule($module))
+       return new Response($app['twig']->render('index.html.twig', array()));
+       //return $app['twig']->render('index.html.twig');
+    else
+        return 'Voce não tem permissão para acessar esta funcionalidade';
+})->value ( 'module', 'modulo' )->bind('homepage');
+
+$app->get( '/{module}/crud/edit/{id}', function ($module, $id) use ($app){
+    return 'GET Controller='.$controller.' e Method= EDIT id='.$id;
+});
+
+$app->get( '/{module}/crud/list', function ($module) use ($app){
+    return 'GET Controller='.$controller.' e Method= LIST ';
+});
+
+
+
+$app->post( '/{module}/crud/create', function ( $module ) use ($app){
+  //verifica a permissão
+    return 'POST = Controller='.$controller.' e Method= Create';
+});
+
+$app->post( '/{module}/crud/update', function ( $module, $controller ) use ($app){
+  //verifica a permissão
+    return 'POST = Controller='.$controller.' e Method= Update';
+});
+
+$app->post( '/{module}/crud/delete/{id}', function ( $module, $controller, $id ) use ($app){
+  //verifica a permissão
+    return 'POST = Controller='.$controller.' e Method= Delete';
+});
+
+$app->get( '{module}/{controller}/{method}', function ( $module, $controller, $method ) use ($app) {
+  //verifica a permissão
+    return ' Another Functions Controller='.$controller.' e Method='.$method;
+});
+
 $app->get( '/', function (Application $app, Request $bar) {
     $class = new Bpms\Teste\Controller\TesteController($app, $bar);
     $resp = $class->getResponse();
@@ -77,9 +142,13 @@ $app->get( '/load/{id}', function (Application $app, Request $bar, $id) {
 
 
 
+*/
+
+
 
 
 
 
 //$app->mount('/', new Bpms\Teste\Controller\TesteControllerMount());
 $app->run();
+return $app;
